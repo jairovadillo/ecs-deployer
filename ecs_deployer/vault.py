77a2648b51a82e3e2c3d96c1f8d492695e6a51fb
acatlla <alex.catlla@21buttons.com>
2019-07-05 11:50:27 +0200

    Adapt deployer to read secrets from vault with KV V2

--- ecs_deployer/vault.py
+++ ecs_deployer/vault.py
@@ -4,6 +4,6 @@ import hvac
 def get_configuration_vars(host: str, token: str, path: str) -> list:
     client = hvac.Client(url=host, token=token)
     response = client.read(path)
-    env_vars = response.get('data')
+    env_vars = response.get('data', {}).get('data')
 
     return [{"name": k, "value": v} for k, v in env_vars.items()]
