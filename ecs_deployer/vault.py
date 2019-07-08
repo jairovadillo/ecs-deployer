e599d8ea8bfd4af017dd6702e7fd48281d4d7c9d
acatlla <alex.catlla@21buttons.com>
2019-07-08 11:17:23 +0200

    Compatible KV V1 and KV V2

--- ecs_deployer/vault.py
+++ ecs_deployer/vault.py
@@ -4,6 +4,13 @@ import hvac
 def get_configuration_vars(host: str, token: str, path: str) -> list:
     client = hvac.Client(url=host, token=token)
     response = client.read(path)
-    env_vars = response.get('data', {}).get('data')
+    resp_data = response.get('data')
+
+    # Compatible KV V1 and KV V2
+    if resp_data and 'data' in resp_data and 'metadata' in resp_data:
+        env_vars = resp_data.get('data')
+    else:
+        env_vars = resp_data
+
 
     return [{"name": k, "value": v} for k, v in env_vars.items()]
