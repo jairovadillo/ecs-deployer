c4a26a25112a7d529054b4bc29ede01815382ff7
acatlla <alex.catlla@21buttons.com>
2019-07-08 11:28:07 +0200

    Check KV V2 length response

--- ecs_deployer/vault.py
+++ ecs_deployer/vault.py
@@ -4,13 +4,13 @@ import hvac
 def get_configuration_vars(host: str, token: str, path: str) -> list:
     client = hvac.Client(url=host, token=token)
     response = client.read(path)
-    resp_data = response.get('data')
+    data = response.get('data')
 
     # Compatible KV V1 and KV V2
-    if resp_data and 'data' in resp_data and 'metadata' in resp_data:
-        env_vars = resp_data.get('data')
+    if data and len(data) == 2 and 'data' in data and 'metadata' in data:
+        env_vars = data.get('data')
     else:
-        env_vars = resp_data
+        env_vars = data
 
 
     return [{"name": k, "value": v} for k, v in env_vars.items()]
